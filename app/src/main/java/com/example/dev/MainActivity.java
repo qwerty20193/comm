8ada9576f389806a6c673d8e3b355287208b0819
qwerty20193 <nikitakrutov17@gmail.com>
2021-08-18 13:26:24 +0300

    comm
    comm

---
+++ app/src/main/java/com/example/dev/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.annotation.TargetApi;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        webView.loadUrl("https://0560.ru/corporate/services/rsa/");
+
+        setContentView(webView);
+    }
+}
